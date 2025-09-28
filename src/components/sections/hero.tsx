d5d302d197c0ae5be2b41123442591b8dcac77da
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-09-28 21:12:43 +0100

    Feat: fix hero and navbar

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -7,13 +7,13 @@ const HeroSection = () => {
   return (
     <section className="relative h-screen bg-[url('/hero-1.webp')] bg-cover bg-center">
       {/* Container */}
-      <div className="w-[90%] max-w-[1200px] mx-auto h-full flex items-center">
+      <div className="w-[90%] max-w-[1350px] mx-auto h-full flex items-center">
         {/* Left content */}
-        <div className="flex flex-col gap-6 max-w-[450px] text-white z-10">
-          <h1 className="text-[52px] font-bold leading-tight">
+        <div className="flex flex-col gap-6 max-w-[450px] text-white z-10 mt-[200px]">
+          <h1 className="text-[55px] font-bold leading-tight">
             Get Rewarded For The Internet You Don’t Use
           </h1>
-          <p className="text-lg text-gray-200">
+          <p className="text-[25px] text-gray-200">
             Grass turns your unused internet into rewards automatically.
           </p>
 
@@ -33,15 +33,15 @@ const HeroSection = () => {
       </div>
 
       {/* Bottom right trusted badge */}
-      <div className="absolute bottom-20 right-12 flex items-center gap-3 bg-white rounded-full shadow-md px-4 py-2">
+      <div className="absolute bottom-[80px] right-[180px] flex items-center gap-3 bg-white rounded-full shadow-md px-4 py-2">
         <div className="flex -space-x-2">
           {people.map((src, i) => (
             <Image
               key={i}
               src={src}
               alt="User avatar"
-              width={32}
-              height={32}
+              width={42}
+              height={42}
               className="rounded-full border-2 border-white"
             />
           ))}
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,6 @@
 export const Button = ({ title, className, variant }: ButtonProps) => {
     // const widths = bidWidth ? "min-w-[150px] text-[16px[" : "min-w-[100px] text-[13px]"
-    const variants = variant === "white" ? "bg-white text-black font-semibold" : variant === "green" ? "bg-green-500 text-black font-semibold" : "bg-black text-white"
+    const variants = variant === "white" ? "bg-white text-black font-semibold" : variant === "green" ? "bg-lime-400 text-black font-semibold" : "bg-black text-white"
     return (
         <button className={`${className} ${variants} cursor-pointer rounded-[50px] text-center inline-flex uppercase py-[10px] px-[30px]`}>{title}</button>
     )
