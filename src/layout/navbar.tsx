24999b04b5ca496d76b51f903321330fb29f4db5
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-09-28 20:56:27 +0100

    fix: hero section

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -23,17 +23,25 @@ const Navbar = () => {
   return (
     <nav
       className={`fixed top-5 left-1/2 transform -translate-x-1/2 z-50 flex items-center justify-between w-[90%] transition-colors duration-300 rounded-[50px] min-h-[60px] py-[15px] px-[30px] ${
-        scrolled ? "bg-white shadow-bottom border border-black" : "bg-transparent"
+        scrolled ? "bg-white shadow-bottom border border-black" : "bg-transparent text-white"
       }`}
     >
         <div className="flex gap-20">
         <div className="relative">
-        <Image
+        
+        {scrolled ? (<Image
         src="/logo.webp"
         alt="Logo"
         width={130}
         height={130}
+        />) : (
+        <Image
+        src="/logo-white.webp"
+        alt="Logo"
+        width={130}
+        height={130}
         />
+        )}
 
         <span className="absolute top-0 right-[-25px] text-[12px] font-semibold">TM</span>
         </div>
@@ -46,7 +54,7 @@ const Navbar = () => {
 
         <Button 
         title="Open Dashboard"
-        className={`min-w-[150px] text-[16px]`}
+        className={`min-w-[150px] text-[16px] border ${scrolled ? "border-none" : "border-white bg-transparent"}`}
         />
     </nav>
   )
