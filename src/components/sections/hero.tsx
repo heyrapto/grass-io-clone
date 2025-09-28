48c09dd496bdda05f570a3668c1d34b6cdf901ca
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-09-28 21:19:55 +0100

    fix: hero section

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -7,9 +7,10 @@ const HeroSection = () => {
   return (
     <section className="relative h-screen bg-[url('/hero-1.webp')] bg-cover bg-center">
       {/* Container */}
-      <div className="w-[90%] max-w-[1350px] mx-auto h-full flex items-center">
+      <div className="w-[80%] mx-auto h-full flex items-end pb-[150px] justify-between">
+        
         {/* Left content */}
-        <div className="flex flex-col gap-6 max-w-[450px] text-white z-10 mt-[200px]">
+        <div className="flex flex-col gap-6 max-w-[450px] text-white z-10">
           <h1 className="text-[55px] font-bold leading-tight">
             Get Rewarded For The Internet You Don’t Use
           </h1>
@@ -30,25 +31,25 @@ const HeroSection = () => {
             />
           </div>
         </div>
-      </div>
 
-      {/* Bottom right trusted badge */}
-      <div className="absolute bottom-[80px] right-[180px] flex items-center gap-3 bg-white rounded-full shadow-md px-4 py-2">
-        <div className="flex -space-x-2">
-          {people.map((src, i) => (
-            <Image
-              key={i}
-              src={src}
-              alt="User avatar"
-              width={42}
-              height={42}
-              className="rounded-full border-2 border-white"
-            />
-          ))}
+        {/* Right trusted badge */}
+        <div className="flex items-center gap-3 bg-white rounded-full shadow-md px-5 py-3">
+          <div className="flex -space-x-2">
+            {people.map((src, i) => (
+              <Image
+                key={i}
+                src={src}
+                alt="User avatar"
+                width={42}
+                height={42}
+                className="rounded-full border-2 border-white"
+              />
+            ))}
+          </div>
+          <span className="text-sm font-medium text-gray-800">
+            Trusted by over 3M users worldwide
+          </span>
         </div>
-        <span className="text-sm font-medium text-gray-800">
-          Trusted by over 3M users worldwide
-        </span>
       </div>
     </section>
   );
